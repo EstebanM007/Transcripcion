5313cabe6f772be12cf68ac490776e7391baa7cd
EstebanM007 <eamartinezp@unbosque.edu.co>
2025-04-11 15:33:28 -0500

    Modificacion de codigo para importar por Ruta

--- mp4 to wav.py
+++ mp4 to wav.py
@@ -1,7 +1,7 @@
 from moviepy.editor import VideoFileClip
 
 # Cargar el archivo .mp4
-video_clip = VideoFileClip("audio.mp4")
+video_clip = VideoFileClip(r"D:\38514603\Videos\Grabaciones de pantalla\Grabación de pantalla 2025-04-11 140938.mp4")
 
 # Extraer el audio y guardarlo como .wav
 audio_clip = video_clip.audio
